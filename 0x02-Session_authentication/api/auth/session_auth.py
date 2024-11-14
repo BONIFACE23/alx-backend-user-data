2542d25840fa1d1dc45d6e18c0e0e2df4ab5ad64
root <irunguboniface413@gmail.com>
2024-11-14 13:43:49 +0000

    session_auth.py

---
+++ 0x02-Session_authentication/api/auth/session_auth.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Model for th object SessionAuth """
+from api.v1.auth.auth import Auth
+from models.user import User
+from uuid import uuid4
+
+
+class SessionAuth(Auth):
+    """ Class to Authorized sessions that inherits from Auth """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for a user_id """
+        if type(user_id) != str:
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns a User ID based on a Session ID """
+        if type(session_id) != str:
+            return None
+
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
+
+    def current_user(self, request=None):
+        """ Returns a User instance based on a cookie value """
+        my_session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(my_session_id)
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ Deletes the user session / logout """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
